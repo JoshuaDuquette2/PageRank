f227a2d00da4090284961e66352b8756babe4882
JoshuaDuquette2 <113274399+JoshuaDuquette2@users.noreply.github.com>
2023-07-08 18:08:04 -0700

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        //Category:Java_platform
+        NetworkBuilder builder = new NetworkBuilder("Java_(programming_language)");
+    }
+}
